f6b7d4ca74c8bdba5bb8fc69da004f7b09a9dac9
jasmit21 <jasmitrathod21@gmail.com>
2022-09-15 09:34:40 +0530

    .

--- controllers/cluedinApprecieveMessageController.js
+++ controllers/cluedinApprecieveMessageController.js
@@ -2,7 +2,7 @@ const con = require("../models/dbConnect");
 
 module.exports = {
     get : (req,res)=>{
-        getMessageSql = "SELECT message_id,userName,userRole,message_title,message_label_id,user_message,isExpired,dateOfExpiration,image_url,dateOfDeletion,category from cluedin.user_message where isDelete = 0";
+        getMessageSql = "SELECT message_id,userName,userRole,message_title,message_label_id,user_message,image_url,dateOfCreation from cluedin.user_message where isDelete = 0";
         con.query(getMessageSql,(err,result)=>{
             res.json({"notifications": result});
         })
