9ed1850f6c0db20935f011fa09a5c631dcb5df39
jasmit21 <jasmitrathod21@gmail.com>
2023-01-04 03:41:33 +0530

    included logic for getting selected fcm tokens

--- controllers/homeNotifController.js
+++ controllers/homeNotifController.js
@@ -11,6 +11,7 @@ module.exports = {
     // firebaseAdmin.initializeApp({
     //   credential: firebaseAdmin.credential.cert(serviceAccount),
     // });
+    var session = req.session;
 
     var notif_title = req.body.notif_title;
     // console.log(notif_title);
@@ -19,36 +20,73 @@ module.exports = {
     var scheduled_date = req.body.scheduled_date;
     var category = req.body.category;
     var label = req.body.label;
-
-
-
+    var target_class = req.body.target_class;
+    var gender = req.body.user_gender;
 
     // var sql = "INSERT INTO user_message (title,message,expDate,schDate,category) VALUES ?";
+    console.log("sessionid", session.userid);
     var sql =
-      "INSERT INTO user_message (message_title,user_message,dateOfExpiration,scheduled_date,category,message_label) VALUES ?";
-    var values = [
-      [notif_title, notif_desc, exp_date, scheduled_date, category, label],
-    ];
+      "INSERT INTO notification_message (nm_title,sender_id,nm_message,nm_label_id) VALUES ?";
+    var values = [[notif_title, session.senderid, notif_desc, label]];
     pool.query(sql, [values], (err, result) => {
-
-
-      if (err) res.send(err);
+      if (err) throw err;
 
       // res.send("notif sent");
       console.log("data inserted finally!!!");
     });
 
+    //logic for getting selected fcm tokens
 
+    var target_gender = gender;
+    var params = {};
 
+    if (target_gender != 0) {
+      params.target_gender = target_gender;
+    }
 
+    console.log(params);
+
+    function buildConditions(params) {
+      var conditions = [];
+      var values = [];
+      var conditionsStr;
+
+      if (typeof params.target_gender !== "undefined") {
+        conditions.push("t1.user_gender = ?");
+        values.push(parseInt(params.target_gender));
+      }
+
+      if (1) {
+        conditions.push(
+          `t2.user_id = t1.user_id and t2.ay_id=2 and t2.bsd_id ="${target_class}" and t2.isDisabled=0 and t2.isDelete=0;`
+        );
+        // values.push(parseInt(params.target_gender));
+      }
+
+      return {
+        where: conditions.length ? conditions.join(" AND ") : "1",
+        values: values,
+      };
+    }
+
+    var conditions = buildConditions(params);
+    var sql_1 =
+      "select t1.firebase_token from user_details t1, Student_branch_standard_div_ay_rollno_sem_mapping t2 WHERE " +
+      conditions.where;
+
+    console.log(sql_1);
+    pool.query(sql_1, conditions.values, (err, result) => {
+      if (err) res.send(err);
+
+      // res.send("notif sent");
+      console.log("om", result);
+    });
 
     var getFcmTokensSql = [
       "cghubDBUQVixer-83NkN9n:APA91bFQyrRyUNsDCR3_V1wMUViHZO3Tgst7lVTeWez6CT5J7-hekMEGgvdI26tptTu1UQUvagP-ovYd2ObpXyH0QXpOHWk4D-oUjRPPFK2jWkUO8xq0ye8y8C0Pnbm9DGmyaYIbp1gb",
       "f4AXXfzaQ_6_7LsOaD7DtC:APA91bF80tU46SGXZB1rmqdpki5Agxsv-d4a047KYNI0lOYFHc7kjqVcCRYgkprEqPG-u82WKGduMvwu_xi3HLD_USA079E7CHKV67pjN3isu7mAUI6BR-_LBpzmttJHTrZI-0r-iBWw",
       "cGg34Ul2RYeHkAI4qYOWLX:APA91bFnkiHQniXzaDTCGevv_3bmyZQ8yDBMQudGqn5LwcVqfYS_yxfjoerUaTryTbrES282pVLu79WKeA9Nh-PATsNoZv-D96gcv8VNYUozR37_mhrhQc08K3TlxoYJFmGC2t6dk49b",
     ];
-    // pool.query(getFcmTokensSql,(err,result)=>{
-    // if(err) throw err;
 
     const payload = {
       notification: {
@@ -72,22 +110,13 @@ module.exports = {
     firebaseAdmin.messaging().sendToDevice(getFcmTokensSql, payload, options);
 
     req.flash("message1", "Notification Sent ");
+
+    //query for notification_message_targetlist
+    qry = `INSERT INTO notification_message_targetlist (nm_id,bsd_id,nm_gender) values ((SELECT nm_id from notification_message WHERE nm_title = "${notif_title}" ),"${target_class}","${gender}")`;
+    pool.query(qry,(err,result)=>{
+      if (err) throw err;
+      log("inserted into notif target table")
+    });
     res.redirect("/dashboard");
-
-    
-    // res.send("ok");
-
-    // data[fcmToken()];
-    // console.log(fcmToken());
-    // ----------------------firebase notification-------------------------
-    // const fcmToken = [
-    //     "ebjzXqouTCeRUMTvWlwP0e:APA91bEcYr-AaP8xNftjJx0ATqtpgQ4_ucGOwm_JfTyTPkJunhFakRxpqqFJrjbo9kMbQ7cQC1_H43IgK3y0dOQFtMOKBuV2cUZfUrwYoIxGpJeh62oNmN5uRVAIFb5vfHYOWKeA1EMM",
-    //     "dUNs1KbMTTuC9IJ1YN-QNN:APA91bEtf_3484eeJQDboSsV-I7Fddn3JjAKtpUsuQoMamaVAr3x9MTEqksoJolOPgQyQE8KUR_3-Eoy9AFX2XmoxyDbyqExrrCkEg_-_qfC4lJ_K_0uHHQTqFnWcrRSjrMB9ncMDmmq",
-    // ];
-
-    //         //  res.send("notification send");
-    //     }
-
-    // ye dono nikaalna badmai
   },
 };
